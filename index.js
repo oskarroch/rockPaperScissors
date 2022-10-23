a06ba538d4cd9f8bbc64d254d857abca03d7e140
Oskarrro03 <oskarrochowiak@gmail.com>
2022-10-23 17:52:39 +0200

    think its finished

--- index.js
+++ index.js
@@ -1,43 +1,49 @@
-let z;
 let myTimeout;
 let bot;
+let player;
 let clicked = false;
 
 $("button").on("click", () => {
+  winner();
   setTimeout(() => {
     $("#blueBg").animate({ left: "0" });
     $("#redBg").animate({ right: "0" });
-  }, 1000);
+    newGame();
+  }, 1500);
   if (clicked != true) {
     botChoice();
   }
-
-  startGame();
+  $("#leftHand").animate({ left: "-100%" });
+  setTimeout(() => {
+    $("#leftHand").animate({ left: "-300px" });
+  }, 1000);
 });
 
 $("#rockButton").on("mouseover", () => {
   if (clicked != true) {
-    z = "rockLeft";
+    player = "rockLeft";
   }
 });
 
 $("#paperButton").on("mouseover", () => {
   if (clicked != true) {
-    z = "paperLeft";
+    player = "paperLeft";
   }
 });
 
 $("#scissorsButton").on("mouseover", () => {
   if (clicked != true) {
-    z = "scissorsLeft";
+    player = "scissorsLeft";
   }
 });
 
 $("button").on("mouseover", () => {
-  $("#leftHand").removeAttr("class");
-  $("#leftHand").addClass(z);
-  $("#leftHand").animate({ left: "0" });
-  clearTimeout(myTimeout);
+  if (clicked != true) {
+    $("#leftHand").removeAttr("class");
+    $("#leftHand").addClass(player);
+    $("#leftHand").animate({ left: "0" });
+    clearTimeout(myTimeout);
+  }
 });
 
 $("button").on("mouseleave", () => {
@@ -61,13 +67,39 @@ function botChoice() {
     } else if (bot === 2) {
       $("#rightHand").addClass("scissorsRight");
     }
-    $("#rightHand").animate({ right: "0" });
+    $("#rightHand").animate({ right: "-200px" });
   }, 1000);
 }
 
-function startGame() {
-  $("#leftHand").animate({ left: "-100%" });
+function newGame() {
   setTimeout(() => {
-    $("#leftHand").animate({ left: "0" });
-  }, 1000);
+    clicked = false;
+    $("#blueBg").animate({ left: "-100%" });
+    $("#redBg").animate({ right: "-100%" });
+    $("#leftHand").animate({ left: "-100%" });
+    $("#rightHand").animate({ right: "-100%" });
+  }, 4000);
+}
+
+function winner() {
+  setTimeout(() => {
+    if (bot === player) {
+      return;
+    } else if (
+      (bot === 0 && player === "paperLeft") ||
+      (bot === 1 && player === "scissorsLeft") ||
+      (bot === 2 && player === "rockLeft")
+    ) {
+      $("#point").addClass("pointLeft");
+    } else if (
+      (bot === 0 && player === "scissorsLeft") ||
+      (bot === 1 && player === "rockLeft") ||
+      (bot === 2 && player === "paperLeft")
+    ) {
+      $("#point").addClass("pointRight");
+    }
+  }, 2500);
+  setTimeout(() => {
+    $("#point").removeAttr("class");
+  }, 3000);
 }
